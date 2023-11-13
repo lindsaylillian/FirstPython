92bad9726a4ab2202c089ef2737b0f273f6842f9
lindsaylillian <150136754+lindsaylillian@users.noreply.github.com>
2023-11-13 13:07:37 +0300

    invoice

--- bill_invoice.py
+++ bill_invoice.py
@@ -1,1 +1,10 @@
-b
+print("WELCOME TO THE TIP CALCULATOR")
+
+Totalbill=float(input("what was the total bill?"))
+
+percentagetip=input("what was the percentage tip you ould like to give? [10,12 or 15]?\n")
+
+people=input("how many people to split the bill?")
+tip=int(Totalbill)*int(percentagetip)/100
+eachperson=int(Totalbill)+int(tip)/int(people)
+print(eachperson)
