3e1dbe7ac53e5054a8537e12c76cb1b56f5eac2d
lindsaylillian <150136754+lindsaylillian@users.noreply.github.com>
2023-11-14 21:47:13 +0300

    Update expiry_period.py

--- expiry_period.py
+++ expiry_period.py
@@ -1,1 +1,15 @@
 
+currentage=input("what is the current age\n?")
+
+finalage=input("what is the expected age?")
+
+#calculates the years left
+yearsleft=int(finalage)-int(currentage)
+
+#calculates the months left 
+monthsleft=int(yearsleft*12)
+
+#calculates the days left 
+daysleft=int(monthsleft*365)
+print(f"you have{yearsleft} years left,{monthsleft} months and,{daysleft}daysleft")
+
