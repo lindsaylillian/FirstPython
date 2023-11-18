739a7861f100c8950235d569347e9ab9fc924cce
lindsaylillian <150136754+lindsaylillian@users.noreply.github.com>
2023-11-18 21:13:33 +0300

    Update calculatorprogram_2.0.py
    a calculator program using if elif and for loops

--- calculatorprogram_2.0.py
+++ calculatorprogram_2.0.py
@@ -1,1 +1,19 @@
+operation=input("what calculation operation would you like to put in add,sub")
+
+if operation=="add":
+    x=int(input("how many values would you like to add"))
+    sum=0
+    for i in range(x):
+        sum+=int(input("put in the values"))
+    print(sum)
+
+elif operation=="sub":
+    x=int(input("what is the highest value"))
+    y=int(input("how many values would youlike to subtract"))
+    subtract=x
+    for i in range(y):
+        subtract-=int(input("what are the values"))
+    print(subtract)
+
+
 
